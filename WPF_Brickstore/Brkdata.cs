8b0b7cc6d25bfb3cf1d37157c56f0052f4647df1
PLACKO135 <papplaszlolevente@gmail.com>
2024-09-06 11:56:52 +0200

    added class and datagrid

---
+++ WPF_Brickstore/Brkdata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Brickstore
+{
+    internal class Brkdata
+    {
+        int ItemID { get; set; }
+        string ItemName { get; set; }
+        string CategoryName { get; set; }
+        string ColorName { get; set; }
+        int Qty { get; set; }
+
+        public Brkdata(int itemID, string itemName, string categoryName, string colorName, int qty)
+        {
+            ItemID = itemID;
+            ItemName = itemName;
+            CategoryName = categoryName;
+            ColorName = colorName;
+            Qty = qty;
+        }
+
+     
+    }
+}

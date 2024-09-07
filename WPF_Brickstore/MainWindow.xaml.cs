b44bc78cebc358347f7581c10d6c634e47d1cee8
Papp László <papplaszlolevente@gmail.com>
2024-09-07 15:43:36 +0200

    Fallen asleep yesterday before finishing so i did it now

--- WPF_Brickstore/MainWindow.xaml.cs
+++ WPF_Brickstore/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
+using Microsoft.Win32;
+
 
 namespace WPF_Brickstore
 {
@@ -19,42 +21,54 @@ namespace WPF_Brickstore
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-
     {
+        private List<Brkdata> _brickDataList;
 
         public MainWindow()
-
         {
-
             InitializeComponent();
             ReadFileToDataGrid();
-
         }
-
 
         private void ReadFileToDataGrid()
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "BSX files (*.bsx)|*.bsx";
+            openFileDialog.Title = "Select a BSX file";
 
-            List<Brkdata> brickDataList = new List<Brkdata>();
-
-            XDocument xaml = XDocument.Load("brickstore_parts_4643-1-power-boat-transporter.bsx");
-            foreach (var elem in xaml.Descendants("Item"))
+            if (openFileDialog.ShowDialog() == true)
             {
-                string id = elem.Element("ItemID").Value;
-                string nev = elem.Element("ItemName").Value;
-                string kategoria = elem.Element("CategoryName").Value;
-                string szinnev = elem.Element("ColorName").Value;
-                int mennyiseg = int.Parse(elem.Element("Qty").Value);
+                _brickDataList = new List<Brkdata>();
 
+                XDocument xaml = XDocument.Load(openFileDialog.FileName);
+                foreach (var elem in xaml.Descendants("Item"))
+                {
+                    string id = elem.Element("ItemID").Value;
+                    string nev = elem.Element("ItemName").Value;
+                    string kategoria = elem.Element("CategoryName").Value;
+                    string szinnev = elem.Element("ColorName").Value;
+                    int mennyiseg = int.Parse(elem.Element("Qty").Value);
 
-                Brkdata data = new Brkdata(id, nev, kategoria, szinnev, mennyiseg);
-                brickDataList.Add(data);
+                    Brkdata data = new Brkdata(id, nev, kategoria, szinnev, mennyiseg);
+                    _brickDataList.Add(data);
+                }
+
+                drgBrick.ItemsSource = _brickDataList;
             }
-
-            drgBrick.ItemsSource = brickDataList;
-
         }
 
+     
+
+        private void txtSearchbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchText = txtSearchbox.Text.ToLower();
+            var filteredData = _brickDataList.Where(x =>
+                x.ItemID.ToLower().Contains(searchText) ||
+                x.ItemName.ToLower().Contains(searchText) ||
+                x.CategoryName.ToLower().Contains(searchText));
+
+            drgBrick.ItemsSource = filteredData;
+        }
     }
 
 
